ffe9082f9434a50e32cd96be67cf762d7297921b
QQ喵 <gqqnb2005@gmail.com>
2021-11-18 15:30:35 +0800

    Test duplicate edges

--- test_Graphs.py
+++ test_Graphs.py
@@ -66,32 +66,120 @@ def canonicalizeTest3():
 	Program.showGraph(m2, False)
 
 
-def assignEdgeLabel(m):
+def assignEdgeLabel(m, p):
+	"""
+
+	:param m:
+	:param p: probability of assigning a duplicate edge label.
+	:return:
+	"""
 	size = len(m)
 	c = 0
+	rand = random.Random()
 	for i in range(size):
 		for j in range(size):
 			if m[i][j] == 1:
-				m[i][j] = chr(ord('a') + c)
-				c += 1
+				if c > 0 and rand.random() <= p:
+					k = rand.randrange(c)
+					m[i][j] = chr(ord('a') + k)
+				else:
+					m[i][j] = chr(ord('a') + c)
+					c += 1
 			else:
 				m[i][j] = None
 
 
-# def canonicalizeTest4():
-# 	m = [
-# 		[None, None, None, None, None, None, None, None],
-# 		['a', None, None, None, None, None, None, None],
-# 		[None, 'b', None, None, None, None, None, None],
-# 		[None, None, 'c', None, None, None, None, None],
-# 		['d', None, None, None, None, None, None, None],
-# 		[None, None, None, None, 'e', None, None, None],
-# 		['f', None, None, None, None, None, None, None],
-# 		['g', None, None, None, None, None, None, None]]
-#
-# 	Program.prettyPrint(m)
-# 	m2 = Program.canonicalize(m, 2)
-# 	Program.prettyPrint(m2)
+def test_duplicateEdgeLabels():
+	labeledNodeCount = 2
+	m1 = [
+		[None, None, 'c', None, 'c', None, None, None],
+		['a', None, None, None, None, None, None, None],
+		[None, 'e', None, None, None, None, None, None],
+		[None, None, 'f', None, None, None, None, None],
+		['d', None, None, None, None, None, None, None],
+		[None, None, None, None, 'e', None, None, None],
+		['f', None, None, None, None, None, None, None],
+		['a', None, None, None, None, None, None, None]]
+
+	m2 = m1.copy()
+	#
+	m2 = Program.swap(m2, 3, 2)
+	m2 = Program.swap(m2, 2, 5)
+	m2 = Program.swap(m2, 3, 7)
+	m2 = Program.swap(m2, 4, 2)
+	m2 = Program.swap(m2, 5, 2)
+	#
+	# Program.prettyPrint(m2)
+	# Program.canonicalize(m2, labeledNodeCount)
+	# print('----------------')
+	# Program.prettyPrint(m2)
+
+	# size = len(m2)
+	# for c in range(5):
+	# 	i = random.randrange(labeledNodeCount, size)
+	# 	j = random.randrange(labeledNodeCount, size)
+	# 	print(f'swap {i}, {j}')
+	# 	m2 = Program.swap(m2, i, j)
+
+	assert Program.getHash(Program.canonicalize(m1, labeledNodeCount)) == Program.getHash(Program.canonicalize(m2, labeledNodeCount)), 'm2 is \n' + Program.pretty(m2)
+
+
+def test_duplicateEdgeLabels2():
+	labeledNodeCount = 2
+	m1 = [
+		[None, None, 'c', None, 'c', None, None, None],
+		['a', None, None, None, None, None, None, None],
+		[None, 'e', None, None, None, None, None, None],
+		[None, None, 'f', None, None, None, None, None],
+		['d', None, None, None, None, None, None, None],
+		[None, None, None, None, 'e', None, None, None],
+		['f', None, None, None, None, None, None, None],
+		['a', None, None, None, None, None, None, None]]
+
+	m2 = m1.copy()
+	m2 = Program.swap(m2, 6, 7)
+	m2 = Program.swap(m2, 2, 4)
+	m2 = Program.swap(m2, 2, 6)
+	m2 = Program.swap(m2, 4, 7)
+	m2 = Program.swap(m2, 3, 7)
+	#
+	# Program.prettyPrint(m2)
+	# Program.canonicalize(m2, labeledNodeCount)
+	# print('----------------')
+	# Program.prettyPrint(m2)
+
+	# size = len(m2)
+	# for c in range(5):
+	# 	i = random.randrange(labeledNodeCount, size)
+	# 	j = random.randrange(labeledNodeCount, size)
+	# 	print(f'swap {i}, {j}')
+	# 	m2 = Program.swap(m2, i, j)
+
+	assert Program.getHash(Program.canonicalize(m1, labeledNodeCount)) == Program.getHash(Program.canonicalize(m2, labeledNodeCount)), 'm2 is \n' + Program.pretty(m2)
+
+
+def test_duplicateEdgeLabelsRandom():
+	labeledNodeCount = 2
+	m1 = [
+		[None, None, 'c', None, 'c', None, None, None],
+		['a', None, None, None, None, None, None, None],
+		[None, 'e', None, None, None, None, None, None],
+		[None, None, 'f', None, None, None, None, None],
+		['d', None, None, None, None, None, None, None],
+		[None, None, None, None, 'e', None, None, None],
+		['f', None, None, None, None, None, None, None],
+		['a', None, None, None, None, None, None, None]]
+
+	m2 = m1.copy()
+
+	size = len(m2)
+	for c in range(5):
+		i = random.randrange(labeledNodeCount, size)
+		j = random.randrange(labeledNodeCount, size)
+		print(f'swap {i}, {j}')
+		m2 = Program.swap(m2, i, j)
+
+	assert Program.getHash(Program.canonicalize(m1, labeledNodeCount)) == Program.getHash(Program.canonicalize(m2, labeledNodeCount)), 'm2 is \n' + Program.pretty(m2)
 
 
 def test_canonicalizeHashRandom():
@@ -99,7 +187,7 @@ def test_canonicalizeHashRandom():
 	G = nx.generators.scale_free_graph(8, delta_out=0.1)
 	A = nx.adjacency_matrix(G)
 	m = A.toarray().tolist()
-	assignEdgeLabel(m)
+	assignEdgeLabel(m, 0.3)
 
 	# Program.showGraph(m, False)
 
@@ -171,8 +259,29 @@ def test_canonicalizeHash1():
 	Program.showGraph(m2, False, title='m2', layout=nx.planar_layout)
 
 
-# m2 = Program.canonicalize(m1, 2)
-# Program.showGraph(m2, False)
+def test_noOutgoingEdges():
+	m1 = [
+		[None, 'a', None, None, None, None, None, None],
+		['b', None, 'c', None, None, None, None, 'd'],
+		[None, None, None, None, None, None, None, None],
+		['e', None, None, None, None, None, None, None],
+		['f', None, None, None, None, None, 'g', None],
+		['h', None, None, None, None, None, None, None],
+		[None, None, None, None, None, None, None, None],
+		[None, None, None, None, None, None, None, None],
+	]
+
+	m2 = [
+		[None, 'a', None, None, None, None, None, None],
+		['b', None, None, None, None, 'd', 'c', None],
+		[None, None, None, None, None, None, None, None],
+		['h', None, None, None, None, None, None, None],
+		['f', None, 'g', None, None, None, None, None],
+		[None, None, None, None, None, None, None, None],
+		[None, None, None, None, None, None, None, None],
+		['e', None, None, None, None, None, None, None],
+	]
+	assert Program.getHash(Program.canonicalize(m1, 2)) == Program.getHash(Program.canonicalize(m2, 2))
 
 
 if __name__ == '__main__':
