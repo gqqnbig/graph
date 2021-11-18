945cefc8d0045bb65edf81f86ca98b74c1de32db
QQ喵 <gqqnb2005@gmail.com>
2021-11-18 21:55:24 +0800

    Assert the return value

--- test_Easy.py
+++ test_Easy.py
@@ -18,7 +18,9 @@ def test_SameChildren():
 
 	# Program.showGraph(m, True)
 
-	Easy.canonicalize(m, [0])
+	l = [0]
+	Easy.canonicalize(m, l)
+	assert l[1] == 3 or l[1] == 4, '0 points to 3 and 4, and 3,4 are identical. We can choose either.'
 
 
 if __name__ == '__main__':
