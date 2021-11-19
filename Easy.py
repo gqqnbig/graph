a24c3d00359ff903c156bbf7807c4049e909fe02
QQ喵 <gqqnb2005@gmail.com>
2021-11-19 11:56:45 +0800

    Fix for a graph with long loop to an initially known node

--- Easy.py
+++ Easy.py
@@ -30,46 +30,48 @@ def formPath(head, nexts):
 	return paths
 
 
-def findMinNext(m, n, l, visitedPaths=None):
-	if visitedPaths is None:
-		visitedPaths = []
+def findMinNext(m, n, visitedPaths):
 	edges = getOutgoingEdges(m, n)
-	edges = list(filter(lambda t: t[1] not in l, edges))
+	# edges = list(filter(lambda t: t[1] not in l, edges))
 	edges = list(filter(lambda t: (n, t[1]) not in visitedPaths, edges))
 
 	if len(edges) == 0:
 		return None
-	else:
-		# convert to list
-		edges = [([a[0]], [a[1]]) for a in edges]
-		loop = True
-		while True:
-			edges = getMinSet(edges)
-			if loop:
-				if len(edges) > 1:
-					loop = False
-					for i in range(len(edges)):
-						res = findMinNext(m, edges[i][1][-1], l, visitedPaths + formPath(n, edges[i][1]))
-						if res is not None:
-							edges[i] = (edges[i][0] + res[0], edges[i][1] + res[1])
-							loop = True
-				else:
-					break
+
+	# convert to list
+	edges = [([a[0]], [a[1]]) for a in edges]
+	loop = True
+	while True:
+		edges = getMinSet(edges)
+		if loop:
+			if len(edges) > 1:
+				loop = False
+				for i in range(len(edges)):
+					res = findMinNext(m, edges[i][1][-1], visitedPaths + formPath(n, edges[i][1]))
+					if res is not None:
+						edges[i] = (edges[i][0] + res[0], edges[i][1] + res[1])
+						loop = True
 			else:
-				print(f'From {n}, two branches are identical')
 				break
-		return edges[0]
+		else:
+			print(f'From {n}, two branches are identical')
+			break
+	return edges[0]
 
 
 def canonicalize(m, l: List[int]):
 	loop = True
+	visitedPaths = []
 	while loop:
 		loop = False
 		i = 0
 		while i < len(l):
-			r = findMinNext(m, l[i], l)
+			r = findMinNext(m, l[i], visitedPaths)
 			if r is not None:
-				l.append(r[1][0])
+				nextNode = r[1][0]
+				if nextNode not in l:
+					l.append(nextNode)
+				visitedPaths.append((l[i], nextNode))
 				loop = True
 
 			i += 1
--- test_Easy.py
+++ test_Easy.py
@@ -42,5 +42,24 @@ def test_SelfLoop():
 	assert l[1] == 3 or l[1] == 4, '0 points to 3 and 4, and 3, 4 are identical. We can choose either.'
 
 
+@pytest.mark.timeout(5)
+def test_LongLoopToKnown():
+	m = [
+		[None, None, None, 'a', 'a', None, None],
+		['d', None, None, None, None, None, None],
+		[None, None, None, None, None, None, 'c'],
+		[None, None, None, None, None, 'b', None],
+		[None, None, 'b', None, None, None, None],
+		[None, 'c', None, None, None, None, None],
+		['a', None, None, None, None, None, None]
+	]
+
+	# Program.showGraph(m, True)
+
+	l = [0]
+	Easy.canonicalize(m, l)
+	assert l == [0, 4, 2, 6, 3, 5, 1], '[0]-a->[4]-b->[2]-c->[6]-a->[0], 6 goes to 0 via a, which is smaller than 1. Thus 4 should be the smallest node.'
+
+
 if __name__ == '__main__':
 	test_SameChildren()
